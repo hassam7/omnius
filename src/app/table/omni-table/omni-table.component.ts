ae60083fe859fc90825ce8887672d66a14f4e7f4
Hassam Ali <hassam.ali@keeptruckin.com>
2020-03-15 02:06:16 +0500

    add query params

--- src/app/table/omni-table/omni-table.component.ts
+++ src/app/table/omni-table/omni-table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges, ViewEncapsulation } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'omni-table',
@@ -9,8 +10,11 @@ import { Component, OnInit, Input, OnChanges, SimpleChanges, ViewEncapsulation }
 export class OmniTableComponent implements OnInit, OnChanges {
   data: {[key: string]: any};
   @Input() tableData: {[key: string]: any};
-  @Input() shouldShowPagination = true;
-  constructor() { }
+  @Input() shouldShowPagination = false;
+  get queryParams() {
+    return {};
+  }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     this.data = this.tableData;
@@ -20,4 +24,7 @@ export class OmniTableComponent implements OnInit, OnChanges {
     this.data = this.tableData;
   }
 
+  public updateQueryParams() {
+    this.router.navigate([this.router.url], { queryParams: this.queryParams, replaceUrl: true });
+  }
 }
